10cd35b043ba9c580d8c5a6d215f2915c68928d7
Burcu Kamilcelebi <burcucelebi33@gmail.com>
2025-03-17 12:49:45 +0300

    Initial Commit

---
+++ EcoPlate Market/EcoPlate_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoPlate_MarketApp.swift
+//  EcoPlate Market
+//
+//  Created by Burcu Kamilçelebi on 17.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct EcoPlate_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
